e569371da01124585c036d64ae8c949a212c9fbf
Microzz861 <nourasia727@hotmail.com>
2023-09-17 17:38:12 +0300

    Added category and task score UI

--- Javascript Files/Dasher1.js
+++ Javascript Files/Dasher1.js
@@ -55,6 +55,13 @@ function strokeChange(SVG , percent, Number , taskNum , count )
 
 
 window.addEventListener("DOMContentLoaded", (event) => {
+
+
+
+
+
+
+
   const incompletedSVG = document.getElementById('js-icircle');
   const incompletePercent = document.getElementById('js-ipercent');
   const incomTaskNumber = document.getElementById('js-complete-amount');
@@ -76,5 +83,76 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const totalTask = document.getElementById('js-total');
   totalTask.innerHTML = AllTasks; 
 
+document.querySelector('header').innerHTML=`<div class="contain">
+<a href="#" ><img src="images/task.png" class="offlogo"> <span class="titleName"> Taskify </span></a> 
+ <button  class="js-changePfp workButton">
+    <img src="images/Microzz.jpg" alt="Microzz dead" class="pfp">
+   </button>
+ 
+</div>`;
+
+document.querySelector('.js-sidebar').innerHTML=`
+<link rel="stylesheet" href="CSS Files/Sidebar.css">
+
+<nav class="js-Nav" id="js-NavBody">
+<div class="container">
+<ul>
+
+  <li>
+    <div class="side-Button-Wrapper">
+      <button class="bPosition" id="js-Side"><img src="images/category.svg"
+          class="js-Category sideButtonIMG"></button>
+      <a id="js-spanThing" class="name"><span> Categories </span></a>
+
+    </div>
+  </li>
+
+  
+  <li> <a>
+      <div class="gridCategory">
+        <img src="images/signin.png">
+        <span class="name"> Login </span>
+      
+      </div>
+      <hr>
+    </a>
+    
+  </li>
+  
+  
+ 
+
+  <li> <a>
+      <div class="gridCategory" style="margin-top: 20px;">
+        <img src="images/addT.jpg" alt="lol" id="fill-icon-task">
+        <span class="name"> Tasks </span>
+      </div>
+    </a>
+  </li>
+
+  <li><a>
+      <div class="gridCategory" >
+        <img src="images/calendar.jpg">
+        <span class="name"> Calendar </span>
+      </div>
+    </a>
+  </li>
+
+  </ul>
+  <div class="logo" id="js-logo-placement">
+  <a href="https://www.google.com" class="js-position-Logo">
+    
+      <img src="images/task.png">
+      <span class="name" id="xarveIsLegend"> Taskify </span>
+      
+    </a>
+  </div>
+    
+
+
+
+</div>
+</nav>`;
+  
 });
 
